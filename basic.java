2d7059b38f5d81db5145d7e1c0b479f69be8ed53
MADESH2112 <madeshkannan2112@gmail.com>
2026-06-18 17:06:46 +0530

    added new probelem

--- basic.java
+++ basic.java
@@ -1,4 +1,5 @@
 //addition
+import java.util.Scanner;
 public class basic{
     public static void main(String[]args)
     {
@@ -6,5 +7,16 @@ public class basic{
         int num2=30;
         int sum=num1+num2;
         System.out.println("sum = "+sum);
+        
+        // Write a program that takes an integer, then a string, then a char from the user and prints them in the screen.
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        String name=sc.next();
+        char ch=sc.next().charAt(0);
+         System.out.println(num);
+          System.out.println(name);
+          System.out.println(ch);
+
     }
 }
+
