9a003472c6eeb7793ab5a63f8b61f91c59cd4469
MADESH2112 <madeshkannan2112@gmail.com>
2026-06-19 09:48:58 +0530

    added program

--- basic.java
+++ basic.java
@@ -17,6 +17,42 @@ public class basic{
           System.out.println(name);
           System.out.println(ch);
 
+
+          //Write a program to check whether a triangle can be formed with the given values for the angles.
+         Scanner scan=new Scanner(System.in);
+         int angle1=scan.nextInt();
+         int angle2=scan.nextInt();
+         int angle3=scan.nextInt();
+         if(angle1+angle2+angle3==18){
+            System.out.println("Traingle can be formed");
+         }
+         else{
+            System.out.println("Traingle cannot be formed");
+         }
+
+         //Write a program using switch case which takes a value and prints the respective Size.
+//If size is 29 then its small
+         Scanner sc2=new Scanner(System.in);
+         int size=sc2.nextInt();
+         switch(size){
+            case 29:
+                System.out.println("small");
+                break;
+            case 30:
+                System.out.println("medium");
+                break;
+            case 38:
+                System.out.println("large");
+                break;
+            case 42:
+                System.out.println("x large");
+                break;
+            default:
+                System.out.println("invalid");
+                break;
+         }
+         }
+
     }
-}
+
 
