3e1b0f31f297a7dfdbb94e359939023b5a678d9f
MADESH2112 <madeshkannan2112@gmail.com>
2026-06-24 18:11:51 +0530

    Added pattern5

---
+++ week2/pattern5.java
@@ -0,0 +1,11 @@
+public class pattern5{
+    public static void main(String[]args){
+        int n=4;
+        for(int i=4;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
